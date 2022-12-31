1d0ae476c8a9e81051e083a428e2a16b1910e49b
paulolima1212 <plima12121984@gmail.com>
2022-12-31 01:43:39 +0000

    feat: zod and hookform configured

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,12 +1,33 @@
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { TextInput, Button } from '@ignite-ui/react'
 
 import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
+const userFormSchema = z.object({
+  userName: z.string().min(1),
+})
+
+type UserFormData = z.infer<typeof userFormSchema>
 
 export function ClaimUserNameForm() {
+  const { register, handleSubmit } = useForm<UserFormData>({
+    resolver: zodResolver(userFormSchema),
+  })
+
+  async function handleUserFormSubmit() {}
+
   return (
-    <Form as={'form'}>
-      <TextInput size={'sm'} prefix='ignite.com/' placeholder='your-user' />
+    <Form as={'form'} onSubmit={handleSubmit(handleUserFormSubmit)}>
+      <TextInput
+        {...register('userName')}
+        size={'sm'}
+        prefix='ignite.com/'
+        placeholder='your-user'
+      />
       <Button size={'sm'} type='submit'>
         Reserve
         <ArrowRight />
