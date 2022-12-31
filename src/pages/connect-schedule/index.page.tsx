1c9d85640e29eb9559ae56b7d283ea4269920f2e
paulolima1212 <plima12121984@gmail.com>
2022-12-31 16:21:40 +0000

    feat: session configured

--- src/pages/connect-schedule/index.page.tsx
+++ src/pages/connect-schedule/index.page.tsx
@@ -2,8 +2,13 @@ import { Heading, Text, MultiStep, Button } from '@ignite-ui/react'
 import { Container, Header } from '../register/styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function ConnectSchedule() {
+  const session = useSession()
+
+  const isAuthenticated = session.status === 'authenticated'
+
   return (
     <Container>
       <Header>
@@ -18,8 +23,13 @@ export default function ConnectSchedule() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant={'secondary'} size={'sm'}>
-            Connect
+          <Button
+            variant={'secondary'}
+            size={'sm'}
+            onClick={() => signIn('google')}
+            disabled={isAuthenticated}
+          >
+            {isAuthenticated ? 'Connected' : 'Connect'}
           </Button>
         </ConnectItem>
         <Button>
