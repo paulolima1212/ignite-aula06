4638070430ae0f1240cf73f38c372b601d8ada30
paulolima1212 <plima12121984@gmail.com>
2022-12-31 14:37:04 +0000

    feat: new cookies add

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const formRegisterSchema = z.object({
   username: z
@@ -42,7 +43,9 @@ export default function Register() {
         name: data.fullname,
       })
     } catch (error) {
-      console.log(error.response.data.message)
+      if (error instanceof AxiosError && error.response?.data?.message) {
+        alert(error.response.data.message)
+      }
     }
   }
 
