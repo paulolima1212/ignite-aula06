25ea12a2efd35a9ef957387dc8b54e35c4ad6d3b
paulolima1212 <plima12121984@gmail.com>
2023-01-02 21:31:31 +0000

    feat: page user created

---
+++ src/pages/schedule/[username]/Confirm-step/styles.ts
@@ -0,0 +1,3 @@
+import { styled } from '@ignite-ui/react'
+
+export const Container = styled('div', {})
---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,62 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { Container, UserHeader } from './styles'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { ScheduleForm } from './Confirm-step'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatar_url: string
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export default function UserProfileSchedule({ user }: ScheduleProps) {
+  console.log(user.avatar_url)
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatar_url} />
+        <Heading>{user.name}</Heading>
+        <Text size={'sm'}>{user.bio}</Text>
+      </UserHeader>
+      <ScheduleForm />
+    </Container>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatar_url: user.avatar_url,
+      },
+    },
+
+    revalidate: 60 * 60 * 24,
+  }
+}
