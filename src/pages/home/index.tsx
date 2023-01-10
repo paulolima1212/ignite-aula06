138fb0ace10ae65ef34bba24f5cffcbc08f6e1a6
paulolima1212 <plima12121984@gmail.com>
2023-01-10 21:19:26 +0000

    feat: next seo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,30 +4,37 @@ import { Container, Hero, Preview } from './styles'
 import previewImg from '../../assets/preview.png'
 import Image from 'next/image'
 import { ClaimUserNameForm } from './components/ClaimUserNameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading as='h1' size={'4xl'}>
-          Uncomplicated Scheduling
-        </Heading>
-        <Text size={'xl'}>
-          connect your calendar and let people schedule meetings in their spare
-          time
-        </Text>
-        <ClaimUserNameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImg}
-          alt='schedule about application'
-          width={827}
-          height={442}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title='Simple Scheduling by Ignite'
+        description='A web page by Ignite'
+      />
+      <Container>
+        <Hero>
+          <Heading as='h1' size={'4xl'}>
+            Uncomplicated Scheduling
+          </Heading>
+          <Text size={'xl'}>
+            connect your calendar and let people schedule meetings in their
+            spare time
+          </Text>
+          <ClaimUserNameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImg}
+            alt='schedule about application'
+            width={827}
+            height={442}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
